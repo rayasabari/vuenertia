1fb4fc8ea74c15a0d8dc69488b20cd301e35ecae
raya <raya@rhp-valuation.com>
2022-10-24 16:29:03 +0700

    first commit

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,38 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/inertia-vue3';
+import { ref } from '@vue/reactivity';
+
+let counter = ref(0);
+
+const increase = () => counter.value++;
+const decrease = () => counter.value--;
+
+
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
+                Dashboard
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200 font-playfair">
+                        You're logged in!
+
+                        <button @click="increase" class="px-3 py-1 rounded-lg bg-emerald-400">+</button>
+                        {{ counter }}
+                        <button @click='decrease' class="px-3 py-1 rounded-lg bg-rose-600">-</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.vue',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+                playfair: ["Playfair", ...defaultTheme.fontFamily.serif],
+                rusian: ["Rusian", ...defaultTheme.fontFamily.serif],
+            },
+        },
+    },
+
+    plugins: [require('@tailwindcss/forms')],
+};
